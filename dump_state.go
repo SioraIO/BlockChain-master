fa5654d659432cb583cd3c98a3d753f12b7159c1
SioraIO <71831071+SioraIO@users.noreply.github.com>
2020-10-20 17:37:40 +0900

    Commit go program files to git
    Commit go program files to git

---
+++ dump_state.go
@@ -0,0 +1,38 @@
+import (
+	"bytes"
+	"encoding/hex"
+	"encoding/json"
+)
+
+type DumpState struct {
+	bytes.Buffer
+}
+
+func (dw *DumpState) UpdateAccount(updatedAccount *acm.Account) error {
+	dw.WriteString("UpdateAccount\n")
+	bs, err := json.Marshal(updatedAccount)
+	if err != nil {
+		return err
+	}
+	dw.Write(bs)
+	dw.WriteByte('\n')
+	return nil
+}
+
+func (dw *DumpState) RemoveAccount(address crypto.Address) error {
+	dw.WriteString("RemoveAccount\n")
+	dw.WriteString(address.String())
+	dw.WriteByte('\n')
+	return nil
+}
+
+func (dw *DumpState) SetStorage(address crypto.Address, key, value []byte) error {
+	dw.WriteString("SetStorage\n")
+	dw.WriteString(address.String())
+	dw.WriteByte('/')
+	dw.WriteString(hex.EncodeToString(key[:]))
+	dw.WriteByte('/')
+	dw.WriteString(hex.EncodeToString(value[:]))
+	dw.WriteByte('\n')
+	return nil
+}
